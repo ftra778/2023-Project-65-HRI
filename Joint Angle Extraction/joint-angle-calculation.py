2fde2e22b61075e7738c199f332e0bf467ad27e3
cyin631 <cyin631@aucklanduni.ac.nz>
2023-05-14 22:54:18 +1200

    Adding robotics-toolbox-python library

---
+++ Joint Angle Extraction/joint-angle-calculation.py
@@ -0,0 +1,11 @@
+# This script will calculate the 2D joint angles between the shoulder and the elbow, and the elbow and the wrist, given the coordinates of the those joints.
+
+# Pepper robot dimensions:
+
+# Shoulder joint to elbow joint = 28 cm 
+# Elbow joint to wrist joint = 27 cm
+
+
+
+urdf_file = "../Datasheets/JULIETTEY20V171.urdf"
+
